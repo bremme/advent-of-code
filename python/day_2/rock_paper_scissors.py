5f31bbb2256746d08e33455aee271cce6989fcc2
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2022-12-06 13:29:06 +0100

    Fix part two day two

--- python/day_2/rock_paper_scissors.py
+++ python/day_2/rock_paper_scissors.py
@@ -5,11 +5,13 @@ my_shapes = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
 
 shape_scores = {"Rock": 1, "Paper": 2, "Scissors": 3}
 
+outcomes = {"X": "lose", "Y": "draw", "Z": "win"}
+
 
 def calculate_score(opponent_shape, my_shape):
     draw_score = 3
     win_score = 6
-    loose_score = 0
+    lose_score = 0
 
     shape_score = shape_scores[my_shape]
 
@@ -17,7 +19,7 @@ def calculate_score(opponent_shape, my_shape):
         return shape_score + draw_score
 
     if my_shape == "Rock" and opponent_shape == "Paper":
-        return loose_score + shape_score
+        return lose_score + shape_score
 
     if my_shape == "Rock" and opponent_shape == "Scissors":
         return win_score + shape_score
@@ -26,30 +28,79 @@ def calculate_score(opponent_shape, my_shape):
         return win_score + shape_score
 
     if my_shape == "Paper" and opponent_shape == "Scissors":
-        return loose_score + shape_score
+        return lose_score + shape_score
 
     if my_shape == "Scissors" and opponent_shape == "Rock":
-        return loose_score + shape_score
+        return lose_score + shape_score
 
     if my_shape == "Scissors" and opponent_shape == "Paper":
         return win_score + shape_score
 
 
+def determine_shape(opponent_shape, outcome):
+    if outcome == "draw":
+        return opponent_shape
+
+    if outcome == "win" and opponent_shape == "Rock":
+        return "Paper"
+
+    if outcome == "win" and opponent_shape == "Paper":
+        return "Scissors"
+
+    if outcome == "win" and opponent_shape == "Scissors":
+        return "Rock"
+
+    if outcome == "lose" and opponent_shape == "Rock":
+        return "Scissors"
+
+    if outcome == "lose" and opponent_shape == "Paper":
+        return "Rock"
+
+    if outcome == "lose" and opponent_shape == "Scissors":
+        return "Paper"
+
+
+def part_one(data):
+    total_score = 0
+
+    for line in data.splitlines():
+        opponent_encrypted_shape, my_encrypted_shape = line.split(" ")
+
+        opponent_shape = opponent_shapes[opponent_encrypted_shape]
+        my_shape = my_shapes[my_encrypted_shape]
+
+        score = calculate_score(opponent_shape, my_shape)
+        print(line, score)
+        total_score += score
+
+    print(f"Total score part 1: {total_score}")
+
+
+def part_two(data):
+    total_score = 0
+
+    for line in data.splitlines():
+        opponent_encrypted_shape, encrypted_outcome = line.split(" ")
+
+        opponent_shape = opponent_shapes[opponent_encrypted_shape]
+        outcome = outcomes[encrypted_outcome]
+
+        my_shape = determine_shape(opponent_shape, outcome)
+
+        score = calculate_score(opponent_shape, my_shape)
+        print(line, score)
+        total_score += score
+
+    print(f"Total score part 2: {total_score}")
+
+
 def main(input_file):
 
     with open(input_file, "r") as fh:
-        total_score = 0
-        for line in fh.read().splitlines():
-            opponent_encrypted_shape, my_encrypted_shape = line.split(" ")
+        data = fh.read()
 
-            opponent_shape = opponent_shapes[opponent_encrypted_shape]
-            my_shape = my_shapes[my_encrypted_shape]
-
-            score = calculate_score(opponent_shape, my_shape)
-            print(line, score)
-            total_score += score
-
-        print(f"Total score: {total_score}")
+    part_one(data)
+    part_two(data)
 
 
 if __name__ == "__main__":
