9bca6e771e4a7ae207c9cb8a0aba5aed866eda4b
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2022-12-13 23:15:48 +0100

    Add unit test day 10

--- python/test/day_10/test_cathode_ray_tube.py
+++ python/test/day_10/test_cathode_ray_tube.py
@@ -1,6 +1,6 @@
 import unittest
 
-from aoc_2022.day_10.cathode_ray_tube import solve_part_one
+from aoc_2022.day_10.cathode_ray_tube import solve_part_one, solve_part_two
 from aoc_2022.utils import utils
 
 DAY = 10
@@ -20,4 +20,40 @@ class TestRopeBridge(unittest.TestCase):
         answer = solve_part_one(lines)
 
         self.assertEqual(answer, 14420)
+
+    def test_solve_part_two_example(self):
+        lines = utils.read_puzzle_input(day=DAY, example=True)
+
+        expected_answer = [
+            "⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫⚪⚪⚫⚫",
+            "⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫⚫⚫⚪⚪⚪⚫",
+            "⚪⚪⚪⚪⚫⚫⚫⚫⚪⚪⚪⚪⚫⚫⚫⚫⚪⚪⚪⚪⚫⚫⚫⚫⚪⚪⚪⚪⚫⚫⚫⚫⚪⚪⚪⚪⚫⚫⚫⚫",
+            "⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫",
+            "⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪",
+            "⚪⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚪⚪⚫⚫⚫⚫⚫",
+        ]
+
+        crt = solve_part_two(lines)
+
+        for row, expected_row in enumerate(expected_answer):
+            row = "".join(crt.frame_buffer[row])
+            self.assertEqual(row, expected_row)
+
+    def test_solve_part_two(self):
+        lines = utils.read_puzzle_input(day=DAY, example=False)
+
+        expected_answer = [
+            "⚪⚪⚪⚫⚫⚫⚪⚪⚫⚫⚪⚫⚫⚫⚫⚪⚪⚪⚫⚫⚪⚪⚪⚫⚫⚪⚪⚪⚪⚫⚫⚪⚪⚫⚫⚪⚫⚫⚪⚫",
+            "⚪⚫⚫⚪⚫⚪⚫⚫⚪⚫⚪⚫⚫⚫⚫⚪⚫⚫⚪⚫⚪⚫⚫⚪⚫⚫⚫⚫⚪⚫⚪⚫⚫⚪⚫⚪⚫⚫⚪⚫",
+            "⚪⚫⚫⚪⚫⚪⚫⚫⚫⚫⚪⚫⚫⚫⚫⚪⚫⚫⚪⚫⚪⚪⚪⚫⚫⚫⚫⚪⚫⚫⚪⚫⚫⚪⚫⚪⚫⚫⚪⚫",
+            "⚪⚪⚪⚫⚫⚪⚫⚪⚪⚫⚪⚫⚫⚫⚫⚪⚪⚪⚫⚫⚪⚫⚫⚪⚫⚫⚪⚫⚫⚫⚪⚪⚪⚪⚫⚪⚫⚫⚪⚫",
+            "⚪⚫⚪⚫⚫⚪⚫⚫⚪⚫⚪⚫⚫⚫⚫⚪⚫⚪⚫⚫⚪⚫⚫⚪⚫⚪⚫⚫⚫⚫⚪⚫⚫⚪⚫⚪⚫⚫⚪⚫",
+            "⚪⚫⚫⚪⚫⚫⚪⚪⚪⚫⚪⚪⚪⚪⚫⚪⚫⚫⚪⚫⚪⚪⚪⚫⚫⚪⚪⚪⚪⚫⚪⚫⚫⚪⚫⚫⚪⚪⚫⚫",
+        ]
+
+        crt = solve_part_two(lines)
+
+        for row, expected_row in enumerate(expected_answer):
+            row = "".join(crt.frame_buffer[row])
+            self.assertEqual(row, expected_row)
 
