e757e63b34d4728ee5c696c6c1b9140d7d0ffee7
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2022-12-18 22:46:39 +0100

    Add unit test day 12

---
+++ python/test/day_12/test_hill_climbin_algorithm.py
@@ -0,0 +1,45 @@
+import unittest
+
+from aoc_2022.day_12.hill_climbing_algorithm import solve_part_one, solve_part_two
+from aoc_2022.utils import utils
+
+DAY = 12
+
+EXPECTED_ANSWER_PART_ONE_EXAMPLE = 31
+EXPECTED_ANSWER_PART_ONE = 408
+EXPECTED_ANSWER_PART_TWO_EXAMPLE = 29
+EXPECTED_ANSWER_PART_TWO = 399
+
+
+class TestHillClimbingAlgorithm(unittest.TestCase):
+    def test_solve_part_one_example(self):
+        lines = utils.read_puzzle_input(day=DAY, example=True)
+
+        answer = solve_part_one(lines)
+
+        self.assertEqual(answer, EXPECTED_ANSWER_PART_ONE_EXAMPLE)
+
+    def test_solve_part_one(self):
+        lines = utils.read_puzzle_input(day=DAY, example=False)
+
+        answer = solve_part_one(lines)
+
+        self.assertEqual(answer, EXPECTED_ANSWER_PART_ONE)
+
+    def test_solve_part_two_example(self):
+        lines = utils.read_puzzle_input(day=DAY, example=True)
+
+        answer = solve_part_two(lines)
+
+        self.assertEqual(answer, EXPECTED_ANSWER_PART_TWO_EXAMPLE)
+
+    def test_solve_part_two(self):
+        lines = utils.read_puzzle_input(day=DAY, example=False)
+
+        answer = solve_part_two(lines)
+
+        self.assertEqual(answer, EXPECTED_ANSWER_PART_TWO)
+
+
+if __name__ == "__main__":
+    unittest.main()
