77aecc6e0cdf732ee534b7f2d41daf12ec0e73aa
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2022-12-27 15:29:02 +0100

    Change of plan, use sets

--- python/aoc_2022/day_17/pyroclastic_flow.py
+++ python/aoc_2022/day_17/pyroclastic_flow.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum, auto
 
 
@@ -18,6 +19,7 @@ class RockGenerator:
         RockType.I_VERTICAL,
         RockType.O,
     ]
+    NUM_ROCK_TYPES = 5
 
     def __init__(self) -> None:
         self.__rock_type_index = 0
@@ -27,10 +29,7 @@ class RockGenerator:
             yield self.next_rock()
 
     def next_rock(self):
-        if self.__rock_type_index >= len(self.ROCK_TYPE_ORDER):
-            self.__rock_type_index = 0
-
-        rock_type = self.ROCK_TYPE_ORDER[self.__rock_type_index]
+        rock_type = self.ROCK_TYPE_ORDER[self.__rock_type_index % self.NUM_ROCK_TYPES]
 
         self.__rock_type_index += 1
 
@@ -71,49 +70,114 @@ class RockGenerator:
             return Rock(grid)
 
 
+class Jet:
+    LEFT = "<"
+    RIGHT = ">"
+
+
+@dataclass
+class Position:
+    row: int
+    column: int
+
+
+class PositionOffset(Position):
+    pass
+
+
 class Rock:
     def __init__(self, grid):
         self.width = len(grid[0])
         self.height = len(grid)
         self.grid = grid
+        self.position = None
 
     def __str__(self):
         return "\n".join(self.grid)
 
 
 class Chamber:
-    def __init__(self, width, height, jet_pattern) -> None:
+    def __init__(self, width, height, jet_pattern, start_offset) -> None:
         self.width = width
         self.height = height
         self.jet_pattern = jet_pattern
+        self.start_offset = start_offset
+
         self.grid = [["."] * self.width for _ in range(self.height)]
 
-    def add_rock(self, rock):
-        pass
+        self.coordinates = set()
 
-    def push_rock_by_jet(self, direction):
-        pass
+        for row in range(self.height):
+            for column in range(self.width):
+                if row == 0:
+                    self.coordinates.add((row, column))
+                    continue
+                if column == 0:
+                    self.coordinates.add((row, column))
+                    continue
+                if column == self.width - 1:
+                    self.coordinates.add((row, column))
+
+        self.rocks = set()
+
+        self._jet_index = 0
+        self.rock = None
+        self.bottom_row = self.height - 1
+
+    def add_rock(self, rock: Rock):
+        self.rock = rock
+        # place rock in chamber
+        # store position of rock in chamber
+        # should the rock store its position or the chamber ?
+        position = Position(
+            row=self.bottom_row - self.start_offset.row, column=self.start_offset.column
+        )
+        self.rock.position = position
+
+    def push_rock_by_jet(self):
+        jet = self.jet_pattern[self._jet_index % len(self.jet_pattern)]
+
+        self._jet_index += 1
+
+        if jet == ">":
+            # check if we can move right
+            if (self.rock.position.column + self.rock.width) >= self.width:
+                return
+            self.rock.position.column += 1
+
+        elif jet == "<":
+            # check if we can move left
+            if (self.rock.position.column - 1) < 0:
+                return
+            self.rock.position.column -= 1
 
     def move_rock_down(self):
-        pass
+        # check if we can move down
+
+        # ideas
+        # make all rock coordinates a set
+        # make chamber walls also a set
+        # perhaps even merge those
 
     def rock_had_come_to_rest(self):
-
+        pass
 
 
 def solve_part_one(lines: list[str], example: bool):
     number_of_rocks = 2200
     chamber_width = 7
     chamber_height = number_of_rocks * 4
-    start_column_offset = 2
-    start_row_offset = 3
+    start_offset = PositionOffset(row=3, column=2)
 
     jet_pattern = lines[0]
 
     generator = RockGenerator()
-    chamber = Chamber(width=chamber_width, height=chamber_height, jet_pattern=jet_pattern)
-
-
+    chamber = Chamber(
+        width=chamber_width,
+        height=chamber_height,
+        jet_pattern=jet_pattern,
+        start_offset=start_offset,
+    )
 
     for rock in generator.rocks(number_of_rocks):
 
@@ -131,7 +195,3 @@ def solve_part_one(lines: list[str], example: bool):
 
 def solve_part_two(lines: list[str], example: bool):
     pass
-
-
-max height of chamber ?
-2200 * 4 = 8800
