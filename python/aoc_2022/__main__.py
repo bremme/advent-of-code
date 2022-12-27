4aafddf5c247aeb9dbb3f28672ef60c60409dd0c
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2022-12-27 12:38:58 +0100

    Improve help text

--- python/aoc_2022/__main__.py
+++ python/aoc_2022/__main__.py
@@ -4,27 +4,56 @@ import logging
 from aoc_2022.app.runner import Runner
 
 
-def _parse_args():
+def _build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="aoc",
         description="CLI interface to run advent of code puzzles.",
     )
-    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Print more (debugging) output."
+    )
 
     subparsers = parser.add_subparsers(dest="cmd")
-    run_parser = subparsers.add_parser("run")
 
-    run_parser.add_argument("-d", "--day", type=int, required=True)
-    run_parser.add_argument("-p", "--part", type=int)
-    run_parser.add_argument("--variant", default="default")
-    run_parser.add_argument("-e", "--example", action="store_true")
+    run_parser = subparsers.add_parser(
+        "run", help="Run a puzzle solution. see `aoc run --help` for more details."
+    )
 
-    run_parser.add_argument("-f", "--file", type=str, dest="input_file")
-    run_parser.add_argument("--debug", action="store_true")
-    run_parser.add_argument("--profile", action="store_true")
-    run_parser.add_argument("--assert", action="store_true", dest="assert_answer")
+    run_parser.add_argument(
+        "-d", "--day", type=int, required=True, help="The puzzle day."
+    )
+    run_parser.add_argument(
+        "-p", "--part", type=int, choices=[1, 2], help="Only run part one or two."
+    )
+    run_parser.add_argument(
+        "--variant", default="default", help="Run an alternative variant."
+    )
+    run_parser.add_argument(
+        "-e", "--example", action="store_true", help="Run using example data."
+    )
 
-    return parser.parse_args()
+    run_parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        dest="input_file",
+        metavar="FILE",
+        help="CHoose an alternative input file",
+    )
+    run_parser.add_argument(
+        "--debug", action="store_true", help="Launch ipdb shell on exception."
+    )
+    run_parser.add_argument(
+        "--profile", action="store_true", help="Profile this run using cProfile."
+    )
+    run_parser.add_argument(
+        "--assert",
+        action="store_true",
+        dest="assert_answer",
+        help="Check the answer(s).",
+    )
+
+    return parser
 
 
 def _setup_logger(verbose):
@@ -40,12 +69,17 @@ def _setup_logger(verbose):
 
 def main():
 
-    args = _parse_args()
+    parser = _build_parser()
+
+    args = parser.parse_args()
 
     _setup_logger(args.verbose)
 
     if args.cmd == "run":
         Runner(args).run()
+
+    if args.cmd is None:
+        parser.print_help()
 
 
 if __name__ == "__main__":
