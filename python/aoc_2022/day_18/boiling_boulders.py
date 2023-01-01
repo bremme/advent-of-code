920d2f1db6b18c950db26ac9e3b7aae25f77e447
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-01-01 18:19:44 +0100

    Optimize day 18 part one solution

--- python/aoc_2022/day_18/boiling_boulders.py
+++ python/aoc_2022/day_18/boiling_boulders.py
@@ -6,14 +6,7 @@ def parse(lines: list[str]) -> list[list[int]]:
     return cubes
 
 
-def solve_part_one(lines: list[str], example: bool):
-    cubes = parse(lines)
-
-    # sides of cube = 6
-    # when does a cube toch
-    # coordinate is the center of the cube I guess
-    # two cubes can only touch on one side
-    # a cube touches if there is all coordinates are the same except one
+def solve_one(cubes):
 
     surface_area = len(cubes) * 6
 
@@ -24,13 +17,108 @@ def solve_part_one(lines: list[str], example: bool):
             #
             x1, y1, z1 = cube
             x2, y2, z2 = other_cube
+
             dx = abs(x1 - x2)
             dy = abs(y1 - y2)
             dz = abs(z1 - z2)
+
             if sum([dx, dy, dz]) == 1:
                 surface_area -= 1
 
     return surface_area
+
+
+def cube_is_connected(cube, other_cube):
+    x1, y1, z1 = cube
+    x2, y2, z2 = other_cube
+
+    dx = abs(x1 - x2)
+    dy = abs(y1 - y2)
+    dz = abs(z1 - z2)
+
+    if sum([dx, dy, dz]) == 1:
+        return True
+
+    return False
+
+
+def solve_two(cubes):
+    # cubes_x = sorted(cubes, key=lambda c: c[0])
+    # cubes_y = sorted(cubes, key=lambda c: c[1])
+    # cubes_z = sorted(cubes, key=lambda c: c[2])
+
+    cubes_x = {}
+    cubes_y = {}
+    cubes_z = {}
+
+    for cube in cubes:
+        x, y, z = cube
+
+        if x not in cubes_x:
+            cubes_x[x] = []
+        if y not in cubes_y:
+            cubes_y[y] = []
+        if z not in cubes_z:
+            cubes_z[z] = []
+
+        cubes_x[x].append(cube)
+        cubes_y[y].append(cube)
+        cubes_z[z].append(cube)
+
+    # dict with x coordinates and all cubes with those
+    # dict with y coordiantes and all cubes with those
+
+    surface_area = len(cubes) * 6
+
+    for cube in cubes:
+        x, y, z = cube
+        # look for touching cubes in x
+        if (x - 1) in cubes_x:
+            for cube_x in cubes_x[x - 1]:
+                if cube_is_connected(cube, cube_x):
+                    surface_area -= 1
+
+        if (x + 1) in cubes_x:
+            for cube_x in cubes_x[x + 1]:
+                if cube_is_connected(cube, cube_x):
+                    surface_area -= 1
+
+        # look for touching cubes in y
+        if (y - 1) in cubes_y:
+            for cube_y in cubes_y[y - 1]:
+                if cube_is_connected(cube, cube_y):
+                    surface_area -= 1
+
+        if (y + 1) in cubes_y:
+            for cube_y in cubes_y[y + 1]:
+                if cube_is_connected(cube, cube_y):
+                    surface_area -= 1
+
+        # look for touching cubes in z
+        if (z - 1) in cubes_z:
+            for cube_z in cubes_z[z - 1]:
+                if cube_is_connected(cube, cube_z):
+                    surface_area -= 1
+
+        if (z + 1) in cubes_z:
+            for cube_z in cubes_z[z + 1]:
+                if cube_is_connected(cube, cube_z):
+                    surface_area -= 1
+
+    return surface_area
+
+
+def solve_part_one(lines: list[str], example: bool):
+    cubes = parse(lines)
+
+    # sides of cube = 6
+    # when does a cube touch?
+    # coordinate is the center of the cube I guess
+    # two cubes can only touch on one side
+    # a cube touches if there is all coordinates are the same except one
+    # return solve_one(cubes)
+
+    return solve_two(cubes)
 
 
 def solve_part_two(lines: list[str], example: bool):
