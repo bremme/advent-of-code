a9642aa2beac338439d07d5d15b25f8d4e3238da
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-01-02 17:42:21 +0100

    Refactoring and further optimization

--- python/aoc_2022/day_19/not_enough_minerals.py
+++ python/aoc_2022/day_19/not_enough_minerals.py
@@ -1,5 +1,4 @@
 import math
-from decimal import MAX_EMAX
 
 
 def parse(lines: list[str]):
@@ -39,6 +38,26 @@ def parse(lines: list[str]):
     return blueprints
 
 
+def can_robot_be_build(costs, robots, resources):
+    wait = 0
+
+    # Can we build this robot?
+    for resource, amount in costs.items():
+        # continue if we already have the resources
+        if resources[resource] >= amount:
+            max(0, wait)
+            continue
+        # If we don't have robots collecting this resource we can never build this robot
+        if robots[resource] == 0:
+            return False, wait
+        # How long will it take to collect the required amount of resource
+        # take the max to deal with negative values -> we already have enough
+        wait = max(wait, math.ceil((amount - resources[resource]) / robots[resource]))
+
+    # we are able to wait for this robot
+    return True, wait
+
+
 def solve_dfs(
     time_remaining,
     robots: dict,
@@ -46,107 +65,97 @@ def solve_dfs(
     robot_costs,
     max_spend_rates: dict,
     cache,
+    geode_at_time,
+    calls=0,
 ):
+    calls += 1
     # not time_remaining
     if time_remaining == 0:
-        return resources["geode"]
-
-    key = tuple([time_remaining, *robots.values(), *resources.values()])
-
-    if key in cache:
-        return cache[key]
+        return resources["geode"], calls
 
     # if we do nothing
     max_amount_geode = resources["geode"] + robots["geode"] * time_remaining
 
+    # Optimization
+    # check if we have already processed a state for the same minute with higher amount of geode
+    if geode_at_time.get(time_remaining, 0) > resources["geode"]:
+        return max_amount_geode, calls
+
+    cache_index = tuple([time_remaining, *robots.values(), *resources.values()])
+
+    if cache_index in cache:
+        return cache[cache_index], calls
+
     # what happend when we build new robots
     for robot_type, costs in robot_costs.items():
-        # do we want to build this robot?
-        # we always want to build geode robots
-        # we don't want / can't? build robots when we are over the max costs?
-        if robot_type != "geode" and robots[robot_type] >= max_spend_rates[robot_type]:
+        # Optimization: Do we need to build this robot?
+        # always build geode collector robots
+        # do we already have enough collector robot to supply our maximum spend rate
+        if robot_type != "geode" and (
+            robots[robot_type] * time_remaining + resources[robot_type]
+        ) >= (max_spend_rates[robot_type] * time_remaining):
             continue
 
-        # how long do we have to wait to build this robot
-        wait = 0
+        can_be_build, wait = can_robot_be_build(costs, robots, resources)
 
+        if can_be_build is False:
+            continue
+
+        time_to_build_robot = 1
+        new_time_remaining = time_remaining - wait - time_to_build_robot
+
+        # If there is no time left, no point in building this robot
+        if new_time_remaining <= 0:
+            continue
+
+        new_robots = robots.copy()
+        new_resources = {}
+
+        # Calculate resources
+        for resource, number_of_robots in new_robots.items():
+            new_resources[resource] = (
+                resources[resource] + (wait + time_to_build_robot) * number_of_robots
+            )
+
+        # spend resource to build robot
         for resource, amount in costs.items():
-            # If we don't have robots collecting this resource we can't build this robot
-            if robots[resource] == 0:
-                break
-            # How long will it take to collect the required amount of resource
-            # take the max to deal with negative values -> we already have enough
-            wait = max(
-                wait, math.ceil((amount - resources[resource]) / robots[resource])
-            )
-        # we are able to wait for this robot (it did not break)
-        else:
-            time_to_build_robot = 1
-            new_time_remaining = time_remaining - wait - time_to_build_robot
+            new_resources[resource] -= amount
 
-            if new_time_remaining <= 0:
-                continue
+        # build the robot
+        new_robots[robot_type] += 1
 
-            new_robots = robots.copy()
-            new_resources = {}
-
-            for resource, number_of_robots in new_robots.items():
-                new_resources[resource] = (
-                    resources[resource]
-                    + (wait + time_to_build_robot) * number_of_robots
-                )
-
-            # spend resource to build robot
-            for resource, amount in costs.items():
-                new_resources[resource] -= amount
-
-            # build the robot
-            new_robots[robot_type] += 1
-
-            # optimization: it makes no sense to have more resources then the max we can spend each turn
-            for resource, max_spend_rate in max_spend_rates.items():
-                new_resources[resource] = min(
-                    new_resources[resource],
-                    max_spend_rate * time_remaining,
-                )
-
-            max_amount_geode = max(
-                max_amount_geode,
-                solve_dfs(
-                    time_remaining=new_time_remaining,
-                    robots=new_robots,
-                    resources=new_resources,
-                    robot_costs=robot_costs,
-                    max_spend_rates=max_spend_rates,
-                    cache=cache,
-                ),
+        # optimization: it makes no sense to have more resources then the max we can
+        # spend each turn
+        for resource, max_spend_rate in max_spend_rates.items():
+            new_resources[resource] = min(
+                new_resources[resource],
+                max_spend_rate * time_remaining,
             )
 
-    cache[key] = max_amount_geode
+        new_max_amount_geode, calls = solve_dfs(
+            time_remaining=new_time_remaining,
+            robots=new_robots,
+            resources=new_resources,
+            robot_costs=robot_costs,
+            max_spend_rates=max_spend_rates,
+            cache=cache,
+            geode_at_time=geode_at_time,
+            calls=calls,
+        )
+        max_amount_geode = max(max_amount_geode, new_max_amount_geode)
 
-    return max_amount_geode
+    cache[cache_index] = max_amount_geode
+    geode_at_time[time_remaining] = max(
+        geode_at_time.get(time_remaining, 0), resources["geode"]
+    )
+
+    return max_amount_geode, calls
 
 
 def solve_part_one(lines: list[str], example: bool) -> int:
     blueprints = parse(lines)
 
-    blueprint_id = 1
-
-    # print(blueprints)
-
-    def can_robot_be_afforded(robot_type, robot_costs, resources):
-        for resource, cost in robot_costs.items():
-            if resources[resource] < cost:
-                return False
-        return True
-
-    def pay_for_robot(robot_cost, resources):
-        for resource, cost in robot_cost.items():
-            resources[resource] -= cost
-        return resources
-
     robots = {"ore": 1, "clay": 0, "obsidian": 0, "geode": 0}
-
     resources = {"ore": 0, "clay": 0, "obsidian": 0, "geode": 0}
 
     total_quality_level = 0
@@ -154,52 +163,16 @@ def solve_part_one(lines: list[str], example: bool) -> int:
     for id_, blueprint in blueprints.items():
         robot_costs = blueprint["robot_costs"]
         max_spend_rates = blueprint["max_spend_rates"]
-        solution = solve_dfs(24, robots, resources, robot_costs, max_spend_rates, {})
+        solution, calls = solve_dfs(
+            24, robots, resources, robot_costs, max_spend_rates, {}, {}
+        )
         quality_level = id_ * solution
-        print(f"quaility level {id_} = {quality_level} ({solution})")
+        print(
+            f"quaility level {id_} = {quality_level} (solutions = {solution}, calls = {calls})"
+        )
         total_quality_level += quality_level
 
     return total_quality_level
-
-    # robot_types_to_build = ["geode", "obsidian", "clay", "ore"]
-
-    # build_robot_type = None
-
-    # for minute in range(1, 24 + 1):
-
-    #     print(f"== Minute {minute} ==")
-
-    #     # determine which robot to build
-    #     for robot_type in robot_types_to_build:
-    #         if can_robot_be_afforded(robot_type, robot_costs[robot_type], resources):
-    #             print(
-    #                 f"Spend {robot_costs[robot_type]} to start building a {robot_type} collector robot"
-    #             )
-    #             resources = pay_for_robot(robot_costs[robot_type], resources)
-    #             build_robot_type = robot_type
-    #             break
-
-    #         build_robot_type = None
-
-    #     # collect resources
-    #     for resource, amount in robots.items():
-    #         resources[resource] += amount
-    #         if amount == 0:
-    #             continue
-    #         print(
-    #             f"{amount} {resource}-collecting robots collect {amount} {resource}; you now have {resources[resource]}."
-    #         )
-
-    #     # finishe building new collectors
-    #     if build_robot_type:
-    #         robots[build_robot_type] += 1
-    #         print(
-    #             f"The new {build_robot_type}-collecting robot is ready; you now have {robots[build_robot_type]} of them."
-    #         )
-    #     if minute == 11:
-    #         return
-
-    return 0
 
 
 def solve_part_two(lines: list[str], example: bool) -> int:
