088dc4fd08e04cea03bb50921824d9c26386511d
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-01-03 21:22:51 +0100

    Add files for day 24

---
+++ python/aoc_2022/day_24/blizzard_basin.py
@@ -0,0 +1,10 @@
+def parse(lines: list[str]):
+    pass
+
+
+def solve_part_one(lines: list[str], example: bool) -> int:
+    pass
+
+
+def solve_part_two(lines: list[str], example: bool) -> int:
+    pass
